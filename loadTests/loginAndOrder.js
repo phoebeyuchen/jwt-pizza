2c14817d36065ac0f5ec975f1e3ca55e8527bd24
Yu Chen <cy928@byu.edu>
2025-11-22 22:06:29 -0700

    add k6 load test for login and order

---
+++ loadTests/loginAndOrder.js
@@ -0,0 +1,186 @@
+import { sleep, check, group, fail  } from 'k6'
+import http from 'k6/http'
+import jsonpath from 'https://jslib.k6.io/jsonpath/1.0.2/index.js'
+
+export const options = {
+  cloud: {
+    distribution: { 'amazon:us:ashburn': { loadZone: 'amazon:us:ashburn', percent: 100 } },
+    apm: [],
+  },
+  thresholds: {},
+  scenarios: {
+    Scenario_1: {
+      executor: 'ramping-vus',
+      gracefulStop: '30s',
+      stages: [
+        { target: 5, duration: '30s' },
+        { target: 15, duration: '1m' },
+        { target: 10, duration: '30s' },
+        { target: 0, duration: '30s' },
+      ],
+      gracefulRampDown: '30s',
+      exec: 'scenario_1',
+    },
+  },
+}
+
+export function scenario_1() {
+  let response
+
+  const vars = {}
+
+  group('page_1 - https://pizza.cy928cs329.click/', function () {
+    // Login
+    response = http.put(
+      'https://pizza-service.cy928cs329.click/api/auth',
+      '{"email":"a@jwt.com","password":"admin"}',
+      {
+        headers: {
+          accept: '*/*',
+          'accept-encoding': 'gzip, deflate, br, zstd',
+          'accept-language': 'en-US,en;q=0.9,zh-TW;q=0.8,zh;q=0.7',
+          'content-type': 'application/json',
+          origin: 'https://pizza.cy928cs329.click',
+          priority: 'u=1, i',
+          'sec-ch-ua': '"Chromium";v="142", "Google Chrome";v="142", "Not_A Brand";v="99"',
+          'sec-ch-ua-mobile': '?1',
+          'sec-ch-ua-platform': '"Android"',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'same-site',
+        },
+      }
+    )
+
+    if (!check(response, { 'status equals 200': response => response.status.toString() === '200' })) {
+      console.log(response.body);
+      fail('Login was *not* 200');
+    }
+    
+    vars['token'] = jsonpath.query(response.json(), '$.token')[0]
+
+    sleep(3.9)
+
+    // Get Menu
+    response = http.get('https://pizza-service.cy928cs329.click/api/order/menu', {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9,zh-TW;q=0.8,zh;q=0.7',
+        authorization: `Bearer ${vars['token']}`,
+        'content-type': 'application/json',
+        'if-none-match': 'W/"1fc-cgG/aqJmHhElGCplQPSmgl2Gwk0"',
+        origin: 'https://pizza.cy928cs329.click',
+        priority: 'u=1, i',
+        'sec-ch-ua': '"Chromium";v="142", "Google Chrome";v="142", "Not_A Brand";v="99"',
+        'sec-ch-ua-mobile': '?1',
+        'sec-ch-ua-platform': '"Android"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+      },
+    })
+
+    // Get Franchise
+    response = http.get(
+      'https://pizza-service.cy928cs329.click/api/franchise?page=0&limit=20&name=*',
+      {
+        headers: {
+          accept: '*/*',
+          'accept-encoding': 'gzip, deflate, br, zstd',
+          'accept-language': 'en-US,en;q=0.9,zh-TW;q=0.8,zh;q=0.7',
+          authorization: `Bearer ${vars['token']}`,
+          'content-type': 'application/json',
+          'if-none-match': 'W/"b5-lW6UfTW2Fcx3F2fMCx1wJqS3B3I"',
+          origin: 'https://pizza.cy928cs329.click',
+          priority: 'u=1, i',
+          'sec-ch-ua': '"Chromium";v="142", "Google Chrome";v="142", "Not_A Brand";v="99"',
+          'sec-ch-ua-mobile': '?1',
+          'sec-ch-ua-platform': '"Android"',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'same-site',
+        },
+      }
+    )
+    sleep(10.3)
+
+    response = http.get('https://pizza-service.cy928cs329.click/api/user/me', {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9,zh-TW;q=0.8,zh;q=0.7',
+        authorization: `Bearer ${vars['token']}`,
+        'content-type': 'application/json',
+        'if-none-match': 'W/"5e-189SFpwNJYW2L9WYRoqdQeAlNNA"',
+        origin: 'https://pizza.cy928cs329.click',
+        priority: 'u=1, i',
+        'sec-ch-ua': '"Chromium";v="142", "Google Chrome";v="142", "Not_A Brand";v="99"',
+        'sec-ch-ua-mobile': '?1',
+        'sec-ch-ua-platform': '"Android"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+      },
+    })
+    sleep(3.5)
+
+    // Purchase Pizza
+    response = http.post(
+      'https://pizza-service.cy928cs329.click/api/order',
+      '{"items":[{"menuId":3,"description":"Margarita","price":0.0042}],"storeId":"1","franchiseId":1}',
+      {
+        headers: {
+          accept: '*/*',
+          'accept-encoding': 'gzip, deflate, br, zstd',
+          'accept-language': 'en-US,en;q=0.9,zh-TW;q=0.8,zh;q=0.7',
+          authorization: `Bearer ${vars['token']}`,
+          'content-type': 'application/json',
+          origin: 'https://pizza.cy928cs329.click',
+          priority: 'u=1, i',
+          'sec-ch-ua': '"Chromium";v="142", "Google Chrome";v="142", "Not_A Brand";v="99"',
+          'sec-ch-ua-mobile': '?1',
+          'sec-ch-ua-platform': '"Android"',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'same-site',
+        },
+      }
+    )
+
+    if (!check(response, { 
+      'purchase status equals 200': response => response.status.toString() === '200' 
+    })) {
+      console.log('Purchase failed with response:', response.body)
+      fail('Purchase was *not* 200')
+    }
+
+    vars['pizzaJWT'] = jsonpath.query(response.json(), '$.jwt')[0]
+
+    sleep(2.5)
+
+    // Verify Pizza
+    response = http.post(
+      'https://pizza-factory.cs329.click/api/order/verify',
+      JSON.stringify({ jwt: vars['pizzaJWT'] }),
+      {
+        headers: {
+          accept: '*/*',
+          'accept-encoding': 'gzip, deflate, br, zstd',
+          'accept-language': 'en-US,en;q=0.9,zh-TW;q=0.8,zh;q=0.7',
+          authorization: `Bearer ${vars['token']}`,
+          'content-type': 'application/json',
+          origin: 'https://pizza.cy928cs329.click',
+          priority: 'u=1, i',
+          'sec-ch-ua': '"Chromium";v="142", "Google Chrome";v="142", "Not_A Brand";v="99"',
+          'sec-ch-ua-mobile': '?1',
+          'sec-ch-ua-platform': '"Android"',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'cross-site',
+          'sec-fetch-storage-access': 'active',
+        },
+      }
+    )
+  })
+}
